46876e8943cdc27163386dc17f73bf9b1ec42a24
SeaCoffee <sapienti8sat@gmail.com>
2024-02-13 17:52:33 +0200

    hw44

---
+++ src/pages/PostPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Post} from "../components/Post/Post";
+
+const PostPage = () => {
+    return (
+        <div>
+            <h1>Post</h1>
+            <Post />
+        </div>
+    )
+}
+
+export default PostPage;
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import {MainLayout} from "./layouts/MainLayout";
 import AlbumsPage from "./pages/AlbumsPage";
 import TodosPage from "./pages/TodosPage";
 import CommentsPage from "./pages/CommentsPage";
+import PostPage from "./pages/PostPage";
 
 
 export const router = createBrowserRouter([
@@ -11,7 +12,7 @@ export const router = createBrowserRouter([
         element: <MainLayout />,
         children: [
             { path: 'albums', element: <AlbumsPage/> },
-            { path: 'comments', element: <CommentsPage/> },
+            { path: 'comments', element: <CommentsPage/>, children: [{path: 'post', element: <PostPage/>}] },
             { path: 'todos', element: <TodosPage/> }
 
         ]
